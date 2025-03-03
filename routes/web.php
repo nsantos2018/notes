bb7c0371582cd6847d6a51b6347172782ac2f355
Natanael Santos <natanael.santos.gomes@gmail.com>
2025-03-03 19:10:20 -0300

    002

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     //return view('welcome');
     echo "<h1>Meu estudo em Laravel!</h1>";
 });
+
+Route::get('/main', [MainController::class, 'index']);
