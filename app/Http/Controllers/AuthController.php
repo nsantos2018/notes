0b3afb28db54c332ef5f81f601184e9c922e023a
Natanael Santos <natanael.santos.gomes@gmail.com>
2025-03-04 21:43:55 -0300

    010

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -18,6 +18,18 @@ class AuthController extends Controller
 
     public function loginSubmit(Request $request)
     {
+        // Form Validation
+        $request->validate(
+            [
+                'text_username' => 'required',
+                'text_password' => 'required'
+            ]
+        );
+
+        // Get User Input
+        $username = $request->input('text_username');
+        $password = $request->input('text_password');
+
         //dd($request);
         echo $request->input('text_username');
         echo '<br>';
